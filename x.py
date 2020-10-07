f51e95402fe51ee113c93f63443769fd844ef668
tirth2212 <52685414+tirth2212@users.noreply.github.com>
2020-10-07 10:00:07 +0530

    Add files via upload

---
+++ x.py
@@ -0,0 +1,15 @@
+import time
+from datetime import datetime
+from data import lst
+import webbrowser
+Started = False
+for i in lst:
+    while True:
+        if Started is False:
+            if datetime.now().hour == int(i[1].split(':')[0]) and \
+                 datetime.now().minute == int(i[1].split(':')[1]):
+                webbrowser.open(i[0])
+                print("Class opened")
+                Started = True
+                Started = False
+                break
