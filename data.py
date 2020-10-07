b02edf4433b86339bf487d133f68f80207eff034
tirth2212 <52685414+tirth2212@users.noreply.github.com>
2020-10-07 10:11:49 +0530

    Update data.py

--- data.py
+++ data.py
@@ -1,4 +1,5 @@
 lst = [
-    ["https://zoom.us/j/95951255535", "9:45"], ["https://zoom.us/j/6525592393", "11:30"],  ["https://zoom.us/j/6525592393", "14:00"], ["", "15:15"]
+    ["https://zoom.us/j/98105909103", "8:30"],["https://zoom.us/j/95951255535", "9:45"], ["https://zoom.us/j/6525592393", "11:30"],  ["https://zoom.us/j/6525592393", "14:00"], ["", "15:15"]
 ]
-# ["https://zoom.us/j/98105909103", "8:30"],
+# This is an Example
+# Use it According to your schedule , just copy paste the links according to the 24H time format 
